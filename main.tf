203c9d1fc412409a2cdd218bfb54369a21a5416d
abansal121 <59279259+abansal121@users.noreply.github.com>
2022-02-06 13:56:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,37 +4,37 @@ provider "aws" {
   region     = us-east-1
 }
 
-resource “aws_vpc” “sandbox-vpc” {
-    cidr_block = “10.0.0.0/16”
-    enable_dns_support = “true” #gives you an internal domain name
-    enable_dns_hostnames = “true” #gives you an internal host name
-    enable_classiclink = “false”
-    instance_tenancy = “default”    
+resource "aws_vpc" "sandbox-vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = "true" #gives you an internal domain name
+    enable_dns_hostnames = "true" #gives you an internal host name
+    enable_classiclink = "false"
+    instance_tenancy = "default"    
     
     tags {
-        Name = “sandbox-vpc”
+        Name = "sandbox-vpc"
     }
 }
 
-resource “aws_subnet” “sandbox-subnet-public-1” {
-    vpc_id = “sandbox-vpc”
-    cidr_block = “10.0.1.0/24”
-    map_public_ip_on_launch = “true” //it makes this a public subnet
-    availability_zone = “eu-west-2a”
+resource "aws_subnet" "sandbox-subnet-public-1" {
+    vpc_id = "sandbox-vpc"
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = "true" //it makes this a public subnet
+    availability_zone = "eu-west-2a"
     tags {
-        Name = “sandbox-subnet-public-1”
+        Name = "sandbox-subnet-public-1"
     }
 }
 
 resource "aws_internet_gateway" "sandbox-igw" {
-    vpc_id = “sandbox-vpc”
+    vpc_id = "sandbox-vpc"
     tags {
         Name = "sandbox-igw"
     }
 }
 
 resource "aws_route_table" "sandbox-public-crt" {
-    vpc_id = “sandbox-vpc”
+    vpc_id = "sandbox-vpc"
     
     route {
         //associated subnet can reach everywhere
@@ -49,13 +49,13 @@ resource "aws_route_table" "sandbox-public-crt" {
 }
 
 resource "aws_route_table_association" "sandbox-crta-public-subnet-1"{
-    subnet_id = “sandbox-subnet-public-1”
+    subnet_id = "sandbox-subnet-public-1"
     route_table_id = "sandbox-public-crt"
 }
 
 resource "aws_security_group" "ec2-web_sg" {
   name   = "ec2_web_sg"
-  vpc_id = “sandbox-vpc”
+  vpc_id = "sandbox-vpc"
 egress {
         from_port = 0
         to_port = 0
@@ -87,7 +87,7 @@ resource "aws_instance" "sandbox_web" {
     ami = "ami-0c2a1acae6667e438"
     instance_type = "t2.micro"
     # VPC
-    subnet_id = “sandbox-subnet-public-1”
+    subnet_id = "sandbox-subnet-public-1"
     # Security Group
     vpc_security_group_ids = ["ec2-web_sg"]
 }
